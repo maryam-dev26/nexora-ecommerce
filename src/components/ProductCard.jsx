11f0312b7cd1ef47452bdec3da2fb268e8004fbc
Maryam <masumaakhtermaryam@gmail.com>
2026-08-22 23:29:06 +0600

    feat: build product card component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
-const ProductCard = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+const ProductCard = ({ product }) => {
+  console.log("my data=", product);
   return (
-    <div>ProductCard</div>
-  )
-}
+    <>
+      <div
+        className="bg-gray-900 rounded-2xl shadow-xl overflow-hidden flex flex-col h-full
+    transition duration-500 transform border border-gray-800 group hover:scale-[1.03]
+    hover:shadow-orange-900/40"
+      >
+        <Link
+          to={"product"}
+          className="relative cursor-pointer overflow-hidden"
+        >
+          <img
+            src={product.image}
+            alt="{product.name}"
+            className="w-full h-56 object-cover object-center 
+            transition duration-500 group-hover:scale-110 group-hover:opacity-90"
+          />
+          <div className="absolute bottom-0 left-0 bg-orange-600/95 text-white px-5 py-2 text-xl
+          font-extrabold rounded-tr-xl shadow-lg">
+             ৳ {product.price.toFixed(2)}
 
-export default ProductCard
+          </div>
+        </Link>
+       
+      </div>
+    </>
+  );
+};
+
+export default ProductCard;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import SearchFilter from "../components/SearchFilter";
 import CategoryFilter from "../components/CategoryFilter";
+import ProductCard from "../components/ProductCard"
 
 import { useCart } from "../Context/CartContext";
 
 const ProductList = () => {
-  console.log("My products =", useCart())
+  const {products} = useCart()
   return (
     <>
       <div className="container mx-auto px-4 md:px-8 pt-8">
         <SearchFilter />
         <CategoryFilter />
+        <h2 className="text-2xl font-extrabold mx-auto px-4 md:px-4 pt-4">
+          Featured Gear ({products.length} Items)
+        </h2>
+        
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8
+        justify-center items-center">
+          {products.map((product , index)=> <ProductCard key={index} product={product} />)}
+        </div>
       </div>
     </>
   );
